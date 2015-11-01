31a80b535fe024bf7697a934dea68a32c559a4e9
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-11-01 15:01:26 +0000

    added upvote and downvote to controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   resources :links
   devise_for :users
-  resources :users
   root to: "links#index"
+
+  resources :links do
+    member do
+      put "like", to: "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
