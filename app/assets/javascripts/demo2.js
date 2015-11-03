cbd008e986fb79ecc833799ff408dee1cf9f9457
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-11-03 09:27:55 +0000

    new day

--- app/assets/javascripts/demo2.js
+++ app/assets/javascripts/demo2.js
@@ -2,26 +2,43 @@ window.onload = function(){
 
 
 	var button = document.getElementById('cn-button'),
-    wrapper = document.getElementById('cn-wrapper');
+	    wrapper = document.getElementById('cn-wrapper'),
+	    overlay = document.getElementById('cn-overlay');
 
-    //open and close menu when the button is clicked
-	var open = false;
-	button.addEventListener('click', handler, false);
+	  //open and close menu when the button is clicked
+	  var open = false;
+	  button.addEventListener('click', handler, false);
+	  button.addEventListener('focus', handler, false);
+	  wrapper.addEventListener('click', cnhandle, false);
 
-	function handler(){
-	  if(!open){
-	    this.innerHTML = "Close";
-	    classie.add(wrapper, 'opened-nav');
+	  function cnhandle(e){
+	    e.stopPropagation();
 	  }
-	  else{
-	    this.innerHTML = "Menu";
-		classie.remove(wrapper, 'opened-nav');
+
+	  function handler(e){
+	    if (!e) var e = window.event;
+	    e.stopPropagation();//so that it doesn't trigger click event on document
+
+	      if(!open){
+	        openNav();
+	      }
+	    else{
+	        closeNav();
+	      }
 	  }
-	  open = !open;
-	}
-	function closeWrapper(){
-		classie.remove(wrapper, 'opened-nav');
-	}
+	  function openNav(){
+	    open = true;
+	      button.innerHTML = "-";
+	      classie.add(overlay, 'on-overlay');
+	      classie.add(wrapper, 'opened-nav');
+	  }
+	  function closeNav(){
+	    open = false;
+	    button.innerHTML = "+";
+	    classie.remove(overlay, 'on-overlay');
+	    classie.remove(wrapper, 'opened-nav');
+	  }
+	  document.addEventListener('click', closeNav);
 }
 
 
