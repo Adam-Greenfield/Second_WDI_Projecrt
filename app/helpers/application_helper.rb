cb31dcf8f50644863fdb218bcc8317ca50fa0efc
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-11-04 10:17:26 +0000

    made the buttons not horrible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def randomized_background_image
-    images = ["https://wallpaperscraft.com/image/star_nebula_glow_94217_1920x1080.jpg", "https://wallpaperscraft.com/image/space_universe_nebula_star_light_95356_1600x1200.jpg", "https://wallpaperscraft.com/image/space_planet_light_82977_1600x1200.jpg", "https://wallpaperscraft.com/image/planet_craters_space_light_61032_1600x1200.jpg"]
+    images = ["https://wallpaperscraft.com/image/star_nebula_glow_94217_1920x1080.jpg", "https://wallpaperscraft.com/image/planet_craters_space_light_61032_1920x1080.jpg", "https://wallpaperscraft.com/image/galaxy_stars_light_nebula_61697_1920x1080.jpg"]
     images[rand(images.size)]
   end
 end
