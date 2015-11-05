200e843f3ea21872893d7be42bd36961bf452d1c
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-11-05 17:16:04 +0000

    about to fill with nonsense

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
+include Faker
+
+100.times do
+  user = User.create ({
+    first_name: Name.first_name
+    second_name: Name.last_name
+    username: App.name
+    bio: Lorem.sentences(3)
+    email: Internet.email
+    created_at: Date.between(12.days.ago, 2.days.ago)
+    updated_at: Date.between(2.days.ago, Date.today)
+    encrypted_password: Lorem.word
+    })
+end
+
+20.times do
+  theme = Theme.create ({
+    name: Hacker.noun
+    description: Lorem.sentences(2)
+    created_at: Date.between(12.days.ago, 2.days.ago)
+    updated_at: Date.between(2.days.ago, Date.today)
+    })
+end
+
+500.times do
+  link = Link.create ({
+    title: Lorem.sentence(3)
+    url: Internet.url
+    created_at: Date.between(12.days.ago, 2.days.ago)
+    updated_at: Date.between(2.days.ago, Date.today)
+    user_id: Number.between(1, 50)
+    theme_id: Number.between(1, 20)
+    })
+end
+
+400.times do
+  comment = Comment.create ({
+    link_id: Number.between(1, 500)
+    user_id: Number.between(1, 100)
+    body: Lorem.sentence(6, true, 6)
+    })
+end
+
--- lib/tasks/populate.rake
+++
@@ -1,8 +0,0 @@
-namespace :db do
-  desc "Erase and fill database"
-  task :populate => :environment do
-    500.times do
-      User.create
-
-  end
-end
